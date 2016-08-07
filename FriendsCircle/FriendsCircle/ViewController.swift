2a07774983115b25948a448b365e3661980b2c05
nguyenanhvu <nganhvu.gon@gmail.com>
2016-08-07 16:55:55 +0700

    checksidebranch

--- FriendsCircle/FriendsCircle/ViewController.swift
+++ FriendsCircle/FriendsCircle/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //First Comment
     }
-    // fkgfkjddd
     
     // hello world
     override func didReceiveMemoryWarning() {
