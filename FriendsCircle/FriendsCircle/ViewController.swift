5b6c6c0bb04f38e88a5792a0ab5b04574f90e02a
Huỳnh Trí Dũng <dunghuynh1990@gmail.com>
2016-08-07 15:46:26 +0700

    initial commit
    Create project

---
+++ FriendsCircle/FriendsCircle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendsCircle
+//
+//  Created by Huynh Tri Dung on 8/7/16.
+//  Copyright © 2016 Huynh Tri Dung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
